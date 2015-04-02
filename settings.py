203e86a50265a33f883c34ad66a2e224dcfaa804
gobrothers <gobrothers@ukr.net>
2015-04-02 18:47:50 +0300

    Changed allowed hosts in settings.py

--- settings.py
+++ settings.py
@@ -168,7 +168,10 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['xn--m1aa7cb.com.ua']
+ALLOWED_HOSTS = [
+    '.xn--m1aa7cb.com.ua',
+    '.nana.in.ua'
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
